dcb3106266d32f15d40b60f6e87adccdead34b32
perky1996 <vbcjvbcj@naver.com>
2018-12-14 03:14:03 +0900

    .

--- app/src/main/java/com/example/vbcjv/start/Next4.java
+++ app/src/main/java/com/example/vbcjv/start/Next4.java
@@ -6,31 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Next3 extends AppCompatActivity {
+public class Next4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_next3);
+        setContentView(R.layout.activity_next4);
 
-        Button b = (Button)findViewById(R.id.button6);
+        Button b = (Button)findViewById(R.id.button8);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(
                         getApplicationContext(), // 현재 화면의 제어권자
-                        Next4.class); // 다음 넘어갈 클래스 지정
+                        MainActivity.class); // 다음 넘어갈 클래스 지정
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
 
-        Button c = (Button)findViewById(R.id.button7);
+        Button c = (Button)findViewById(R.id.button9);
         c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(
                         getApplicationContext(), // 현재 화면의 제어권자
-                        Next2.class); // 다음 넘어갈 클래스 지정
+                        Next3.class); // 다음 넘어갈 클래스 지정
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
